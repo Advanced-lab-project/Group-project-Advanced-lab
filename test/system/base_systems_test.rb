65fc80782d58e85b0c06c4c759e2ca0279a8126b
Rahel Gebeyhu <rahelgebeyhu7@gmail.com>
2026-05-26 19:16:24 +0300

    chore: generate standard system test structure

---
+++ test/system/base_systems_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class BaseSystemsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit base_systems_url
+  #
+  #   assert_selector "h1", text: "BaseSystem"
+  # end
+end
